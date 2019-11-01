c725264a19aed802380b5e664ba337d381128989
Havva Nur <havvanurkaygisiz3@gmail.com>
2019-11-01 15:08:15 +0300

    Update and rename Untitled11.py to 7.Hafta.py

--- 7.Hafta.py
+++ 7.Hafta.py
@@ -1,51 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+
 
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-# In[8]:
-
-
 x=[180,170,170,175,181,175,177,185,179,160]
 y=[95,70,60,79,60,63,83,80,75,50]
 x,y,len(x),len(y)
 
-
-# In[9]:
-
-
 X = np.stack((x, y), axis=0)
 X
-
-
-# In[12]:
-
 
 sigma_1=np.cov(X)
 sigma_1
 
-
-# In[13]:
-
-
 X_test= np.stack((x, x), axis=0)
 X_test
 
-
-# In[14]:
-
-
 sigma_2=np.cov(X_test) #ortalamadan olan farkları carp (varyans)
 sigma_2
-
-
-# In[20]:
-
 
 def generate_data():
     x=[180,170,170,175,181,175,177,185,179,160]
@@ -56,16 +32,8 @@ def get_cov_matrix(X):
     sigma_1=np.cov(X)
     return sigma_1
 
-
-# In[21]:
-
-
 data_1=generate_data()
 get_cov_matrix(data_1)
-
-
-# In[22]:
-
 
 def multivariate_normal(x, d, mean, covariance):
     """pdf of the multivariate normal distribution."""
@@ -74,20 +42,9 @@ def multivariate_normal(x, d, mean, covariance):
             np.exp(-(np.linalg.solve(covariance, x_m).T.dot(x_m)) / 2))
 
 
-# In[23]:
-
-
 x=generate_data()
 
-
-# In[26]:
-
-
 np.mean(x[0,:]),np.mean(x[1,:])  #ortalma boy ve kilo
-
-
-# In[36]:
-
 
 x=generate_data()
 np.mean(x[0,:]),np.mean(x[1,:])
@@ -101,25 +58,10 @@ covariance_1=get_cov_matrix(data)
 multivariate_normal(x_1,d_1,mean_1,covariance_1)
 
 
-# In[ ]:
-
-
-
-
-
-# In[35]:
-
-
 for i in range(10):
     v=167+i
     x_1=[v,72]
     s=multivariate_normal(x_1,d_1,mean_1,covariance_1)
     print(v,"      ",s)
-    
-
-
-# In[ ]:
-
-
 
 
